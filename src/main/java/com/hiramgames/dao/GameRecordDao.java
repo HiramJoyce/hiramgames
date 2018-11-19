7a21ab92bf4949b7b78d72e087f749d459194b41
曹海亮 <1050588656@qq.com>
2018-11-19 15:41:26 +0800

    GameRecord保存完成

--- src/main/java/com/hiramgames/dao/GameRecordDao.java
+++ src/main/java/com/hiramgames/dao/GameRecordDao.java
@@ -1,7 +1,9 @@
 package com.hiramgames.dao;
 
+import com.hiramgames.domain.GameRecord;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class GameRecordDao {
+public interface GameRecordDao {
+    int insertGameRecord(GameRecord gameRecord);
 }
--- src/main/java/com/hiramgames/service/GameRecordService.java
+++ src/main/java/com/hiramgames/service/GameRecordService.java
@@ -1,15 +1,22 @@
 package com.hiramgames.service;
 
+import com.hiramgames.dao.GameRecordDao;
 import com.hiramgames.domain.GameRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 @Service
 public class GameRecordService {
     private final Logger logger = LoggerFactory.getLogger(GameRecordService.class);
 
+    @Resource
+    GameRecordDao gameRecordDao;
+
     public void savaRecord(GameRecord record) {
         logger.info(record.toString());
+        gameRecordDao.insertGameRecord(record);
     }
 }
